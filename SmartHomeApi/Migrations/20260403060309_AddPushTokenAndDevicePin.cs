3cd99b930258ca0341e0ba63c90c369715b2699b
nguyendoquangtrong <23dh113770@st.huflit.edu.vn>
2026-04-03 13:03:23 +0700

    update pin

---
+++ SmartHomeApi/Migrations/20260403060309_AddPushTokenAndDevicePin.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SmartHomeApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPushTokenAndDevicePin : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PushToken",
+                table: "Users",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PushToken",
+                table: "Users");
+        }
+    }
+}
